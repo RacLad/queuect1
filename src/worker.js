7cf821ab09c3f72073d04259e4aeae5b49e321d6
RacLad <raaclad@gmail.com>
2025-11-07 15:05:55 +0530

    loading file

--- src/worker.js
+++ src/worker.js
@@ -4,3 +4,8 @@ import { exec } from "child_process";
 import chalk from "chalk";
 import { fileURLToPath } from "url";
 import { setTimeout as delay } from "timers/promises";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const JOBS_FILE = path.join(__dirname, "../jobs.json");
