51a3ef92087858bbc09278b7040a3877a4a12257
RacLad <raaclad@gmail.com>
2025-11-07 14:08:36 +0530

    writing  json file

--- src/queue.js
+++ src/queue.js
@@ -9,10 +9,15 @@ const __dirname = path.dirname(__filename);
 
 const JOBS_FILE = path.join(__dirname, "../jobs.json");
 
-// Utility function to read JSON safely
+// Utility function to read JSON 
 function readJobsFile() {
   if (!fs.existsSync(JOBS_FILE)) {
     fs.writeFileSync(JOBS_FILE, JSON.stringify({ jobs: [], dlq: [] }, null, 2));
   }
   return JSON.parse(fs.readFileSync(JOBS_FILE));
+}
+
+// Utility function to write JSON 
+function writeJobsFile(data) {
+  fs.writeFileSync(JOBS_FILE, JSON.stringify(data, null, 2));
 }
