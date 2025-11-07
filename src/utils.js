a369263ce52a398d285c4a52d296594b35558422
RacLad <raaclad@gmail.com>
2025-11-07 16:24:36 +0530

    time stamp

--- src/utils.js
+++ src/utils.js
@@ -7,3 +7,11 @@ import crypto from "crypto";
 export function generateJobId() {
   return crypto.randomBytes(8).toString("hex"); // e.g. "a1b2c3d4e5f6g7h8"
 }
+
+/**
+ * Get current timestamp in ISO format
+ * @returns {string} current time
+ */
+export function getCurrentTimestamp() {
+  return new Date().toISOString();
+}
