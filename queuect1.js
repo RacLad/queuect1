26da3868db74053b5e94fa5b47fb50a68ff61f07
RacLad <raaclad@gmail.com>
2025-11-07 13:47:00 +0530

    enqueue command added

--- queuect1.js
+++ queuect1.js
@@ -1,1 +1,28 @@
-console.log("hi there");
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
+import chalk from "chalk";
+import {enqueueJob} from "./src/queue.js"
+import {startWorker} from "./src/worker.js"
+import {showJobs , showDLQ} from "./src/storage.js"
+import { argv } from "process";
+
+const cli = yargs(hideBin(process.argv))
+  .scriptName("queuectl")
+  .usage("Usage: $0 <command> [options]")
+
+// for enqueue command 
+
+.command(
+    "enqueue <jobName>",
+    "Add a new job to the queue",
+    (yargs) => {
+        yargs.positional("jobName",{
+            describe: "Name of the job to enqueue",
+            type: "string",
+        });
+    },
+    async(argv) => {
+        await enqueueJob(argv.jobName);
+        console.log(chalk.green(`Job '${argv.jobName}' added to queue`));
+    }
+)
