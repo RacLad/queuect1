4885dbd605e4947a21269247e7040876d95c6bde
RacLad <raaclad@gmail.com>
2025-11-07 16:21:06 +0530

    creating unique job id

--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,9 @@
 import crypto from "crypto";
+
+/**
+ * Generate a unique job ID
+ * @returns {string} unique job id
+ */
+export function generateJobId() {
+  return crypto.randomBytes(8).toString("hex"); // e.g. "a1b2c3d4e5f6g7h8"
+}
